26e54471044599c751d30a67c481672a8fd34b01
LozanoJohan <lozanojohan321@gmail.com>
2023-09-19 13:42:36 -0500

    Footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'instagram': "url('/src/icons/instagram.svg')",
+				'footer-texture': "url('/img/footer-texture.png')",
+			},
+			keyframes: {
+				rotate1: {
+					'100%': { transform: 'rotate(-3deg)' },
+					'50%': { transform: 'rotate(3deg)' },
+				}
+			},
+			animation: {
+				rotate1: 'rotate1 1s ease-in-out 1s',
+			}
+		},
 	},
 	plugins: [],
 }
